8e41e527de47f5f782a45e74eb65e49b72f0a2fe
jdaguilar <jorgeaguilar2761@gmail.com>
2023-04-24 02:48:19 -0500

    upload missing files in dbt

---
+++ airflow/dbt/capstone_project_dbt/models/fact/fact_gh_archive.sql
@@ -0,0 +1,52 @@
+
+
+{{ config(materialized='table') }}
+
+with staging_gh as (
+  select *
+  from {{ source('bigquery_sources', 'staging_gh_archive')}}
+
+),
+
+dim_event_type as (
+    select * from {{ ref('dim_event_types') }}
+),
+
+dim_organizations as (
+    select * from {{ ref('dim_organizations') }}
+),
+
+dim_repositories as (
+    select * from {{ ref('dim_repositories') }}
+),
+
+dim_users as (
+    select * from {{ ref('dim_users') }}
+),
+
+dim_languages as (
+    select * from {{ ref('dim_languages') }}
+),
+
+select_statement as (
+    select
+        stg.created_at,
+        stg.event_id,
+        de.event_type,
+        dr.repository_id,
+        du.user_id,
+        do.org_id,
+        stg.push_id,
+        stg.number_of_commits,
+        dl.language
+    from staging_gh stg
+    left join dim_languages dl on stg.language = dl.language
+    left join dim_users du on stg.user_id = du.user_id
+    left join dim_repositories dr on stg.repository_id = dr.repository_id
+    left join dim_organizations do on stg.org_id = do.org_id
+    left join dim_event_type de on stg.event_type = de.event_type 
+    order by stg.created_at 
+)
+
+-- Define the final CREATE TABLE statement to create the dim_event_type table
+select * from select_statement
