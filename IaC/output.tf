3fcfbb2d7569bd86e3d7692c37b2c1b41666d954
jdaguilar <jorgeaguilar2761@gmail.com>
2023-04-28 03:04:40 -0500

    return airflow_uri and airflow_dag_folder

--- IaC/output.tf
+++ IaC/output.tf
@@ -6,10 +6,10 @@
 #   value = google_dataproc_cluster.pyspark_cluster.name
 # }
 
-# output "airflow_uri" {
-#   value = google_composer_environment.airflow_service.config.0.airflow_uri
-# }
+output "airflow_uri" {
+  value = module.google_cloud_composer_instance.airflow_uri
+}
 
-# output "airflow_dag_folder" {
-#   value = google_composer_environment.airflow_service.config.0.dag_gcs_prefix
-# }
+output "airflow_dag_folder" {
+  value = module.google_cloud_composer_instance.airflow_dag_folder
+}
