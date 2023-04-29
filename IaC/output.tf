7d58ebc4a6acf4dda8c3a201ef20f8f3dee1d30c
jdaguilar <jorgeaguilar2761@gmail.com>
2023-04-29 00:45:11 -0500

    add cloud_fuction_link and dataproc_cluster_name

--- IaC/output.tf
+++ IaC/output.tf
@@ -1,10 +1,10 @@
-# output "cloud_fuction_link" {
-#   value = google_cloudfunctions_function.cloud_function_instance.https_trigger_url
-# }
+output "cloud_fuction_link" {
+  value = module.google_cloud_function_instance.cloud_fuction_link
+}
 
-# output "dataproc_cluster_name" {
-#   value = google_dataproc_cluster.pyspark_cluster.name
-# }
+output "dataproc_cluster_name" {
+  value = module.google_cloud_dataproc_instance.dataproc_cluster_name
+}
 
 output "airflow_uri" {
   value = module.google_cloud_composer_instance.airflow_uri
