297df98256deeff1aaf1b6f781611b60ed093ccf
jdaguilar <jorgeaguilar2761@gmail.com>
2023-04-29 15:08:51 -0500

    create env_vars.json

--- IaC/modules/google-cloud-composer-instance/main.tf
+++ IaC/modules/google-cloud-composer-instance/main.tf
@@ -1,7 +1,29 @@
 
+resource "local_file" "env_vars" {
+  content  = jsonencode({
+      "DATAPROC_CLUSTER_NUM_WORKERS"           : 1,
+      "DATAPROC_MASTER_DISK_SIZE"              : 300,
+      "DATAPROC_MASTER_MACHINE_TYPE"           : "n2-standard-4",
+      "DATAPROC_CLUSTER_NAME"                  : var.dataproc_cluster_name,
+      "DATAPROC_CLUSTER_ZONE"                  : var.zone,
+      "DATAPROC_PYTHON_SCRIPTS_PATH"           : "gs://${var.pyspark_repo_bucket_name}",
+      "DBT_RUN_JOB_ID"                         : var.dbt_run_job_id,
+      "GOOGLE_CLOUD_PROJECT_ID"                : var.project,
+      "GOOGLE_CLOUD_STORAGE_BUCKET"            : var.data_warehouse_bucket_name,
+      "GOOGLE_CLOUD_STORAGE_DESTINATION_FILES" : "gs://${var.dataproc_cluster_name}/gh-archives/processed/",
+      "GOOGLE_CLOUD_STORAGE_SOURCE_FILES"      : "gs://${var.dataproc_cluster_name}/gh-archives/raw/{0}/*",
+      "REGION"                                 : var.region,
+      "download_gh_data_cloud_function"        : var.cloud_fuction_link
+  })
+  filename = "${path.module}/env_vars.json"
+}
+
+
+
 # Create Airflow Environment
 resource "google_composer_environment" "airflow_service" {
   name   = "capstone-project-airflow-service"
+  project = var.project
   region = var.region
 
   config {
@@ -14,21 +36,6 @@ resource "google_composer_environment" "airflow_service" {
         apache-airflow-providers-dbt-cloud = ""
       }
 
-      env_variables = {
-        # DATAPROC_CLUSTER_NUM_WORKERS = 1,
-        # DATAPROC_MASTER_DISK_SIZE = 300,
-        # DATAPROC_MASTER_MACHINE_TYPE = "n2-standard-4",
-        DATAPROC_CLUSTER_NAME                  = var.dataproc_cluster_name,
-        DATAPROC_CLUSTER_ZONE                  = var.zone,
-        DATAPROC_PYTHON_SCRIPTS_PATH           = "gs://${var.pyspark_repo_bucket_name}",
-        DBT_RUN_JOB_ID                         = var.dbt_run_job_id,
-        GOOGLE_CLOUD_PROJECT_ID                = var.project,
-        GOOGLE_CLOUD_STORAGE_BUCKET            = var.data_warehouse_bucket_name,
-        GOOGLE_CLOUD_STORAGE_DESTINATION_FILES = "gs://${var.dataproc_cluster_name}/gh-archives/processed/",
-        GOOGLE_CLOUD_STORAGE_SOURCE_FILES      = "gs://${var.dataproc_cluster_name}/gh-archives/raw/{0}/*",
-        REGION                                 = var.region,
-        download_gh_data_cloud_function        = var.cloud_fuction_link
-      }
     }
   }
 }
